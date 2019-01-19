7e65422ed33726d55ca2b70e448803d3410b728f
Akataya <infotechpro@yahoo.com>
2019-01-19 18:50:24 +0000

    All tests passed

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,23 @@
-
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
-
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
+        self.following = []
+        self.following_users_posts = []
+    
     def add_post(self, post):
-        pass
-
+        self.posts.append(post)
+        post.user = self
+        post_class = post.__class__
+        post_class.user = post.user
+        
     def get_timeline(self):
-        pass
-
+        posts = []
+        for user in self.following:
+            posts += user.posts
+        return sorted(posts, key=lambda p: p.timestamp, reverse=False)
+        
     def follow(self, other):
-        pass
+        self.following.append(other)
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,41 @@
 from datetime import datetime
 
+class Post(object):
+    def __init__(self, text, timestamp=None, user=None):
+        self.text = text
+        self.timestamp = timestamp.strftime("%A, %b %d, %Y")
+        self.user = user
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
-
-
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def set_user(self, user):
+        self.user = user
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+# TextPost: Just a simple text post. Should be constructed as TextPost(text="Post Text").
+class TextPost(Post):
+    
+# Example: '@Kevin Watson: "Sample post text"\n\tTuesday, Jan 10, 2017'
+    def __str__(self):
+        return '@{first_name} {last_name}: "{post}"\n\t{timestamp}'.format(first_name=self.user.first_name, last_name=self.user.last_name, post=self.text, timestamp=self.timestamp)
+    
 
+# PicturePost: A post containing text and the URL of a picture: Should be constructed as PicturePost(text="Post Text", image_url="imgur.com/OAWTSJu").
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+        super(PicturePost, self).__init__(text, timestamp)
+        self.image_url = image_url
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+        
+#'Example: @Kevin Watson: "Sample post text"\n\thttp://fake-domain.com/images/sample.jpg\n\tTuesday, Jan 10, 2017'
+    def __str__(self):
+        return '@{first_name} {last_name}: "{post}"\n\t{url}\n\t{timestamp}'.format(first_name=self.user.first_name, last_name=self.user.last_name, post=self.text, url=self.image_url, timestamp=self.timestamp)
+
+# CheckInPost: A post containing text and coordinates of the user's position. Should be constructed as CheckInPost(text="Post Text", latitude="40.741895", longitude="-73.989308").
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super(CheckInPost, self).__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+
+# Example: '@Kevin Checked In: "Sample post text"\n\t-34.603722, -58.381592\n\tTuesday, Jan 10, 2017'
+    def __str__(self):
+        return '@{first_name} Checked In: "{post}"\n\t{latitude}, {longitude}\n\t{timestamp}'.format(first_name=self.user.first_name, post=self.text, latitude = self.latitude, longitude = self.longitude, timestamp=self.timestamp)
